ff5d6365079e628a27467aa00ed7b995cc404413
Noah <ndesseri@purdue.edu>
2025-03-10 17:24:13 -0400

    Created BCD Counter - needs test bench

--- source/bcd_counter.sv
+++ source/bcd_counter.sv
@@ -1,7 +1,48 @@
 module bcd_counter
  # (paramater SIZE = 7)
 (
-  output [SIZE-1:0] number,
   input logic second_tick,
+  input clear,
+  input nRST,
+  input clock,
+  output [SIZE-1:0] number
 
 )
+
+logic[SIZE - 1:0] count_reg;
+logic[SIZE - 1:0] next_count_reg;
+logic rollover;
+logic rollover_or_clear;
+logic[SIZE - 1:0] adder_output; 
+
+always_comb begin
+  increment_val = 1;
+
+  adder_output = count_reg;
+
+  if second_tick begin
+    adder_output = count_reg + increment_val;
+  end
+
+  if (count_reg == 99) begin
+    rollover = 1;
+  end
+
+  rollover_or_clear = rollover || clear;
+
+  next_count_reg = !rollover_or_clear && adder_output;
+end
+
+always_ff @(posedge clock, negedge nRST) begin
+  if(!nRST) begin
+    count_reg <= 0; 
+  end
+
+  else begin
+    count_reg <= next_count_reg;
+  end
+end
+
+assign number = count_reg; 
+
+endmodule
