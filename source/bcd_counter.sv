dd6063aca29aa545e1da2da79662fa5c67d54617
Jack Zimmerman <jackzimmerman2006@gmail.com>
2025-03-20 11:10:28 -0400

    fixed syntax and compilation for bcd

--- source/bcd_counter.sv
+++ source/bcd_counter.sv
@@ -1,36 +1,40 @@
 module bcd_counter
- # (paramater SIZE = 7)
+ # (parameter SIZE = 8)
 (
   input logic second_tick,
   input clear,
   input nRST,
   input clock,
   output [SIZE-1:0] number
-
-)
+);
 
 logic[SIZE - 1:0] count_reg;
 logic[SIZE - 1:0] next_count_reg;
 logic rollover;
 logic rollover_or_clear;
 logic[SIZE - 1:0] adder_output; 
+logic[SIZE - 1:0] increment_val;
 
 always_comb begin
   increment_val = 1;
 
   adder_output = count_reg;
 
-  if second_tick begin
+  if (second_tick) begin
     adder_output = count_reg + increment_val;
   end
 
-  if (count_reg == 99) begin
+  if (count_reg == 99) 
     rollover = 1;
-  end
-
+  else 
+    rollover = 0;
+  
   rollover_or_clear = rollover || clear;
 
-  next_count_reg = !rollover_or_clear && adder_output;
+  if (!rollover_or_clear)
+    next_count_reg = adder_output;
+  else
+    next_count_reg = count_reg;
 end
 
 always_ff @(posedge clock, negedge nRST) begin
