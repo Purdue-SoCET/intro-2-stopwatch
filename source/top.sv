b6cf44c1e087eb6e8bd89f1fb5d562dcd1d6a382
Arjun Julius Siderys <asiderys@asicfab.ecn.purdue.edu>
2025-03-27 18:40:14 -0400

    top level fsm instantiation

--- source/top.sv
+++ source/top.sv
@@ -1,19 +1,37 @@
 module top (
-    input logic one_button, ten_button, pause, clear,
+    input logic one_button, ten_button, pause_button, clear_button,
     // add other input/output as necessary
 );
 
-logic one_button_sync, ten_button_sync, pause_sync, clear_sync;
+logic one_button_sync, ten_button_sync, pause_sync, clear_sync; //sync
 
-synch_edge_det sync_one (.clk(clk), .n_rst(n_rst), .async_in(one_button), .edge_flag(one_button_sync));
-
-synch_edge_det sync_ten (.clk(clk), .n_rst(n_rst), .async_in(ten_button), .edge_flag(tem_button_sync));
-
-synch_edge_det sync_pause (.clk(clk), .n_rst(n_rst), .async_in(pause), .edge_flag(pause_sync));
-
-synch_edge_det sync_clear (.clk(clk), .n_rst(n_rst), .async_in(clear), .edge_flag(clear_sync));
+logic ten_run_push, one_run_push, pause_push, clear_push; //fsm 
 
 
+synch_edge_det sync_one (.clk(clk), .n_rst(n_rst), .async_in(one_button), .edge_flag(onestart));
+
+synch_edge_det sync_ten (.clk(clk), .n_rst(n_rst), .async_in(ten_button), .edge_flag(tenstart));
+
+synch_edge_det sync_pause (.clk(clk), .n_rst(n_rst), .async_in(pause_button), .edge_flag(pause));
+
+synch_edge_det sync_clear (.clk(clk), .n_rst(n_rst), .async_in(clear_button), .edge_flag(clear));
+
+fsm fsm_inst1 (.*)
+
+/*
+module fsm
+(
+input logic clk, 
+input logic n_rst,
+input logic onestart,
+input logic tenstart,
+input logic pause,
+input logic clr,
+output logic ten_run_push,
+output logic one_run_push,
+output logic pause_push,
+output logic clear_push
+); */
 
 
 endmodule
