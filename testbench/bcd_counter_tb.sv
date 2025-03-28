aaadd24df348a2986d1e7355baa4f8801927361d
Jack Zimmerman <jackzimmerman2006@gmail.com>
2025-03-28 15:51:21 -0400

    fixed tb

--- testbench/bcd_counter_tb.sv
+++ testbench/bcd_counter_tb.sv
@@ -1,7 +1,7 @@
 module bcd_counter_tb ();
 
     logic [7:0] num;
-    logic enable;
+    logic enable = 1;
     logic clear;
     logic nRST = 1;
     logic clk;
@@ -14,17 +14,33 @@ module bcd_counter_tb ();
 		clk = 1'b0;
 	endtask
 
+    task run_reset();
+        nRST = 0;
+        advance_clock(); 
+        nRST = 1; 
+    endtask
+
     timer t1 (.clk(clk), .enable(enable), .n_rst(nRST), .second_tick(second_tick));
     bcd_counter DUT (.second_tick(second_tick), .clear(clear), .nRST(nRST), .clock(clk), .number(num));
 
     initial begin
         //give us a second tick
-        for (int i=0; i < 20_000_000; i++) begin 
+        $display("Beginning test 1: 10 second ticks");
+        for (int i=0; i < 100_000_000; i++) begin 
             advance_clock();
-            
-            if (second_tick == 1) begin
-                $display("Binary representation: %b, Decimal representation %d%d", num, num[7:4], num[3:0]);
-            end
+        end
+
+        if (num[7:4] == 4'b1 && num[3:0] == 4'b0) begin
+            $display("Passed test 1!");
+        end
+
+    
+        //test 2, reset
+        $display("Beginning test 2: reset");
+        run_reset();
+
+        if (num == 8'b0) begin
+            $display("Passed test 2!");
         end
 
     end
