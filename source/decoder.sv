04afd5897cd06173d9506e2b0fa203e64f1f184d
Jack Zimmerman <jackzimmerman2006@gmail.com>
2025-04-24 18:11:44 -0400

    changed decoder width

--- source/decoder.sv
+++ source/decoder.sv
@@ -1,7 +1,7 @@
 module decoder (
     input  logic [7:0] num,   
-    output logic [6:0] seg1,  
-    output logic [6:0] seg0   
+    output logic [7:0] seg1,  
+    output logic [7:0] seg0   
 );
 
 logic [3:0] tens, units;
@@ -15,17 +15,17 @@ endmodule
 
 module digits(
     input logic [3:0] in,    
-    output logic [6:0] out
+    output logic [7:0] out
 );                           
 
 assign out = ~(
-  (in == 4'd0) ? 7'b0111111 : 
-  (in == 4'd1) ? 7'b0000110 :
-  (in == 4'd2) ? 7'b1011011 : 
-  (in == 4'd3) ? 7'b1001111 : 
-  (in == 4'd4) ? 7'b1100110 : 
-  (in == 4'd5) ? 7'b1101101 : 
-  (in == 4'd6) ? 7'b1111101 : 
-  (in == 4'd7) ? 7'b0000111 : 
-  (in == 4'd8) ? 7'b1111111 : 7'b1100111);
+  (in == 4'd0) ? 8'b0111111 : 
+  (in == 4'd1) ? 8'b0000110 :
+  (in == 4'd2) ? 8'b1011011 : 
+  (in == 4'd3) ? 8'b1001111 : 
+  (in == 4'd4) ? 8'b1100110 : 
+  (in == 4'd5) ? 8'b1101101 : 
+  (in == 4'd6) ? 8'b1111101 : 
+  (in == 4'd7) ? 8'b0000111 : 
+  (in == 4'd8) ? 8'b1111111 : 8'b1100111);
 endmodule
