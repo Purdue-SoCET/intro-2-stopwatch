6688712a3a8ce516d28116da71af6c1ea2e30605
Jack Zimmerman <jackzimmerman2006@gmail.com>
2025-04-24 18:53:03 -0400

    changed clock divider

--- source/timer.sv
+++ source/timer.sv
@@ -1,5 +1,5 @@
 module timer
-	# (parameter FREQUENCY = 100_000_000, parameter TEN_FREQUENCY = 1_000_000_000
+	# (parameter FREQUENCY = 10_000_000, parameter TEN_FREQUENCY = 100_000_000
 	)
 	(
 		input logic clk, 
